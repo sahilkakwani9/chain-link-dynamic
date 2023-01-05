e3b6127597b5c37b66cc2edf8d56832e7cc08b0f
sahilkakwani9 <sahkakwani@gmail.com>
2023-01-05 19:20:12 +0530

    fix: phone

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -31,12 +31,15 @@ function dashboard() {
   return (
     <div className='min-h-screen bg-[#85d7d9]'>
         <div className='text-center'>
-            <h2 className='text-7xl font-One font-bold pt-16 text-gray-700'>Live <span className='underline text-[#6b63de]'>NFT's</span></h2>
-            <div className='flex justify-between items-center px-40 mt-12 flex-wrap gap-y-8'>
+            <div className='flex justify-between h-26 pt-6 px-4'>
+            <h2 className='text-7xl font-One font-bold text-gray-700'>Live <span className='underline text-[#6b63de]'>NFT's</span></h2>
+            <button className='hidden md:block text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-bold rounded-lg text-2xl px-9 py-5 text-center mr-2 mb-2'>Mint New</button>
+            </div>
+            <div className='flex justify-between items-center mt-12 flex-wrap gap-y-8'>
             {
                 nft && supply.map((i)=>{
                    return (
-                    <div className='text-left'>
+                    <div className='text-left mx-auto'>
                     <div className='w-80 h-48'>
                         <img src={nft.image} className='h-[100%] w-full rounded-md shadow-lg shadow-gray-400' />
                     </div>
